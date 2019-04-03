606e1273c6653e1f4e5e36e7dfda717c2899a943
VictoriaDiro <victoriadiro01@gmail.com>
2019-04-03 13:08:11 +0200

    add name preview

--- _src/assets/js/previewData.js
+++ _src/assets/js/previewData.js
@@ -1,1 +1,32 @@
+'use strict';
+
 console.log('preview');
+
+const name = document.getElementById('name');
+const cardName = document.querySelector('.card__name')
+const job = document.getElementById('job');
+const cardJob = document.querySelector('.card__job')
+
+function defaultName() { 
+  const value = name.value;
+  if (value === '') {
+    cardName.innerHTML = 'Nombre Apellido';
+  }
+  else {
+    cardName.innerHTML = value; 
+  }
+}
+
+name.addEventListener('keyup', defaultName);
+
+function defaultJob() { 
+    const value = job.value;
+    if (value === '') {
+      cardJob.innerHTML = 'Front-end developer';
+    }
+    else {
+      cardJob.innerHTML = value; 
+    }
+  }
+  
+  job.addEventListener('keyup', defaultJob);
