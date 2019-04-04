ab8693441148716883a5d2b925be04c712b92aa8
“Sara <saracrojas@gmail.com>
2019-04-04 14:01:49 +0200

    Add variables, add color palette js

--- _src/assets/js/colorPalette.js
+++ _src/assets/js/colorPalette.js
@@ -1,1 +1,23 @@
+'use strict'
+
 console.log('color');
+const cardPreview = document.querySelector ('.preview__card');
+const selection1 = document.getElementById('selection1');
+const selection2 = document.getElementById('selection2');
+const selection3 = document.getElementById('selection3');
+
+function changeColorPalette(class_to_add) {
+    cardPreview.classList.remove('theme1', 'theme2', 'theme3');
+    cardPreview.classList.add(class_to_add);
+    console.log('yasss');
+}
+
+selection1.addEventListener('Change', () => {
+    changeColorPalette('theme1');
+});
+selection2.addEventListener('Change', () => {
+    changeColorPalette('theme2');
+});
+selection3.addEventListener('Change', () => {
+    changeColorPalette('theme3');
+});
