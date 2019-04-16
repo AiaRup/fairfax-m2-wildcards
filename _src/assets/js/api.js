c3da3816d3844eaab3b16b8c0b7f2eedf6c0bfbe
AiaRup <aiale88@gmail.com>
2019-04-16 11:41:56 +0200

    finish fetch request

--- _src/assets/js/api.js
+++ _src/assets/js/api.js
@@ -1,30 +1,50 @@
 'use strict';
 
 const createBtn = document.querySelector('.share__button-create');
+const error = document.querySelector('.error');
+const shareSection = document.querySelector('.share__hidden');
+const form = document.querySelector('.main__settings');
 const url = 'https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card';
 
-const postData = () => {
+const postData = user => {
   console.log('user', userProfile);
   fetch(url, {
     method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
+    body: JSON.stringify(user),
     headers: {
-      'Content-Type': 'application/json',
+      'content-type': 'application/json',
     },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: JSON.stringify(userProfile),
   })
     .then(response => response.json())
     .then(result => {
       console.log('result', result);
+      showURL(result);
     })
     .catch(error => console.log('error', error));
 };
 
-createBtn.addEventListener('click', postData);
+function showURL(result) {
+  const linkElement = document.createElement('a');
+  if (result.success) {
+    shareSection.classList.remove('hidden');
+    const textLink = document.createTextNode(result.cardURL);
+    linkElement.appendChild(textLink);
+    linkElement.setAttribute('href', `${result.cardURL}`);
+    linkElement.setAttribute('class', 'share__link');
+  } else {
+    const textLink = document.createTextNode(`ERROR: ${result.error}`);
+    error.appendChild(textLink);
+  }
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+});
+
+createBtn.addEventListener('click', () => {
+  console.log('in create card');
+  postData(userProfile);
+});
 
 // hide the share section
 // if all fields are valid
