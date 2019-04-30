85916121907f84371bfbeb4e481042e294a2cdc3
LorenaBH <lorenabenitoherrero@gmail.com>
2019-04-30 11:06:41 +0200

    refactoring color palette

--- _src/assets/js/colorPalette.js
+++ _src/assets/js/colorPalette.js
@@ -3,8 +3,11 @@
 const selection2 = document.getElementById('selection2');
 const selection3 = document.getElementById('selection3');
 
+const colorArr = ['theme1', 'theme2', 'theme3'];
+
+
 const changeColorPalette = (event, classToAdd) => {
-  cardPreview.classList.remove('theme1', 'theme2', 'theme3');
+  cardPreview.classList.remove(... colorArr);
   cardPreview.classList.add(classToAdd);
   createBtn.classList.remove('share__button-active');
   startOver();
@@ -13,13 +16,13 @@ const changeColorPalette = (event, classToAdd) => {
 };
 
 selection1.addEventListener('change', event => {
-  changeColorPalette(event, 'theme1');
+  changeColorPalette(event, colorArr[0]);
 });
 
 selection2.addEventListener('change', event => {
-  changeColorPalette(event, 'theme2');
+  changeColorPalette(event, colorArr[1]);
 });
 
 selection3.addEventListener('change', event => {
-  changeColorPalette(event, 'theme3');
+  changeColorPalette(event, colorArr[2]);
 });
